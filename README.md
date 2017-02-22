638f3e09c53f4eaf23c15b5e0fa20fd91a885737
Syanmil <syanmil@yahoo.com>
2017-02-22 14:59:05 +0700

    initial system build

--- README.md
+++ README.md
@@ -4,4 +4,24 @@
   To get basic understanding to yourself more, be more in control and productive person
 
 ### Usage
-  
+
+```
+npm i personality-reading
+
+```
+at your code:
+```
+var reading = require('personality-reading')
+```
+Reading based on your name:
+```
+reading.name('alexander')
+
+// output:
+```
+Reading based on your birthdate:
+```
+reading.birthdate('08/25/1985')
+
+// output:
+```
---
+++ index.js
@@ -0,0 +1,49 @@
+exports.personality-reading = {
+  readName: function(name) {
+    name = name.split('')
+    return name.map(function(data){
+      return data.charCodeAt(0)
+    })
+  },
+  readBirthdate: function(mmddyyyy){
+     return mmddyyyy.match(/\d{2}/g)
+  },
+  reducer: function(array){
+    return array.reduce(function(a, b){
+      return a + b * 7777777
+    })
+  },
+  numberPicker: function(number){
+    return number.match(/\d{2}/g)
+  },
+  summary: function(array){
+    let person = []
+    let mod = 0
+    while (person.length < 6){
+      if(array[mod] > 20 && array[mod] < 95){
+        person.push(array[mod])
+        mod++
+      } else {
+        mod++
+      }
+      if (mod >= array.length){
+        mod = 0
+      }
+    }
+    return {
+      Openness: person[0],
+      Conscientiousness: person[1],
+      Extraversion: person[2],
+      Aggreableness: person[3],
+      Neuroticism: person[4]
+    }
+  },
+  name: function(name){
+    let data = reading.numberPicker(String(reading.reducer(reading.readName(name))))
+    return reading.summary(data)
+  },
+  birthdate: function(date){
+    let data = reading.numberPicker(String(reading.reducer(reading.readBirthdate(date))))
+    return reading.summary(data)
+  }
+}
